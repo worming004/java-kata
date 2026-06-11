07d691daceabce5836422f87a79b99b978876363
Worming <worming@hotmail.be>
2026-06-11 14:04:58 +0200

    Remove Testcontainers bootstrap from integration tests

--- src/test/java/com/kata/library/AbstractIntegrationTest.java
+++ src/test/java/com/kata/library/AbstractIntegrationTest.java
@@ -1,31 +1,13 @@
 package com.kata.library;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MSSQLServerContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 /**
  * Base class for integration tests.
- * Starts a SQL Server Testcontainer once per test class and wires the
- * datasource URL / credentials into the Spring context automatically.
+ *
+ * Tests use the SQL Server instance configured through the standard
+ * Spring datasource properties and do not start containers automatically.
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
 public abstract class AbstractIntegrationTest {
-
-    @Container
-    @SuppressWarnings("resource")
-    static final MSSQLServerContainer<?> SQL_SERVER =
-            new MSSQLServerContainer<>("mcr.microsoft.com/mssql/server:2022-latest")
-                    .acceptLicense();
-
-    @DynamicPropertySource
-    static void datasourceProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", SQL_SERVER::getJdbcUrl);
-        registry.add("spring.datasource.username", SQL_SERVER::getUsername);
-        registry.add("spring.datasource.password", SQL_SERVER::getPassword);
-    }
 }
